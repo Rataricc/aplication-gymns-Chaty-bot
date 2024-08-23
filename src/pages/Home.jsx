874e3e9d3b2901188ea62a7624d5a314e2639b66
Lautaro Rataric <lautarorataric163@gmail.com>
2024-08-23 18:02:16 -0300

    abc03

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,8 @@ export default function Home() {
                             <Nav.Link href="#1">¿Qué hacemos?</Nav.Link>
                             <Nav.Link href="#2">Mensajes</Nav.Link>
                             <Nav.Link href="#3">¿Cómo ingresar?</Nav.Link>
-                            <Nav.Link href="http://localhost:3000/login">Login</Nav.Link>
+                            {/*<Nav.Link href="http://localhost:3000/login">Login</Nav.Link>*/}
+                            <Nav.Link href="https://chaty-gyms.netlify.app/login">Login</Nav.Link>
                         </Nav>
                     
                         <button onClick={toggleDarkMode} className="btn btn-secondary">
