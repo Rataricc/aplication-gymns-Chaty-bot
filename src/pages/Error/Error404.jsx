b2c3e840766f930a6555471c2011630b55ff9d96
Lautaro Rataric <lautarorataric163@gmail.com>
2024-08-26 02:14:34 -0300

    return abc13

--- src/pages/Error/Error404.jsx
+++ src/pages/Error/Error404.jsx
@@ -26,7 +26,7 @@ export default function Error404() {
                         <h3><i className="fas fa-exclamation-triangle text-warning" /> Oops! Page not found.</h3>
                         <p>
                             We could not find the page you were looking for.
-                            Meanwhile, you may <Link to="http://localhost:3000">return to dashboard</Link> or try using the search form.
+                            Meanwhile, you may <Link to="https://chaty-gyms.netlify.app/">return to dashboard</Link>.
                         </p>
                      
                     </div>
